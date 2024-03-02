a283b7fa5d7bacc30798e9eaed37ea28ac2496b3
akent <alexanderkent099@gmail.com>
2024-03-02 14:16:19 -0500

    Implemented logout route handling POST requests to destroy user sessions if logged in. Returned appropriate responses based on session status

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -47,3 +47,16 @@ router.post('/login', async (req, res) => {
         res.status(400).json(err);
     }
 });
+
+// Logout route 
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+module.exports = router;
