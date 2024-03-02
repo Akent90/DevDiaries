c66fd53757d22fc4495bbc4c3808fb95921de8be
akent <alexanderkent099@gmail.com>
2024-03-02 11:42:23 -0500

    Configured Express server with Handlebars templating engine, session management with Sequelize store, middleware for JSON and form data parsing, and static file serving. Started server and synchronized Sequelize with database

--- server.js
+++ server.js
@@ -9,3 +9,36 @@ const SeauelizeStore = require('connect-session-sequelize')(session.Store);
 
 // Automatically import index.js from controllers 
 const routes = require('./controllers');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// Set up access to the sequelize store
+const sess = {
+    secret: process.env.SESSION_SECRET, 
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+};
+
+app.use(session(sess));
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Use routes 
+app.use(routes);
+
+// Start the server and sync sequelize
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on PORT ${PORT}`));
+});
