dbf254be224ef5a787fde3e9c4734c59c9738379
akent <alexanderkent099@gmail.com>
2024-03-02 13:44:38 -0500

    Configured router to handle different routes by importing individual route files for API, homepage, and dashboard. Exported the aggregated router for use in the application

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const exphbs = require('express-handlebars');
 const path = require('path');
 
 const sequelize = require('./config/connection');
-const SeauelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 // Automatically import index.js from controllers 
 const routes = require('./controllers');
