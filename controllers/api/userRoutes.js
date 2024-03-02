1f9d2e15ffbd5f63ee98aace0894a119f537fc6d
akent <alexanderkent099@gmail.com>
2024-03-02 14:13:33 -0500

    Implemented login route handling POST requests to authenticate users. Utilized bcrypt for password comparison and session management. Returned appropriate responses based on success or failure

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { User } = require('../../models');
 const bcrypt = require('bcrypt');
 
+// Sign up route 
 router.post('/signup', async (req, res) => {
     try {
         const userData = await User.create(req.body);
@@ -12,6 +13,35 @@ router.post('/signup', async (req, res) => {
             req.session.loggedIn = true;
 
             res.status(200).json(userData);
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+// Login route 
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: { email: req.body.email } });
+
+        if (!userData) {
+            res.status(400).json({ message: 'Incorrect email or password, please try again' });
+            return;
+        }
+
+        const validPassword = await bcrypt.compare(req.body.password, userData.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect email or password, please try again' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.userId = userData.id;
+            req.session.username = userData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: userData, message: 'You are now logged in!' });
         });
     } catch (err) {
         res.status(400).json(err);
