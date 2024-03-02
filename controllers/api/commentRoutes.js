a000cf6bd61f9120ebd9de2d324b7b1d8b727f01
akent <alexanderkent099@gmail.com>
2024-03-02 15:09:53 -0500

    Implemented route to delete a comment by its id value

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -27,3 +27,23 @@ router.post('/', withAuth, async (req, res) => {
         res.status(400).json(err);
     }
 });
+
+// Delete a comment 
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const commentData = await Comment.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+        if (!commentData) {
+            res.status(404).json({ message: 'No comment found with this id!' });
+            return;
+        }
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
