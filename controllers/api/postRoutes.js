3ab6a18433117eca7338726fdd2817277362901c
akent <alexanderkent099@gmail.com>
2024-03-02 14:59:06 -0500

    Implemented route to handle deleting a post by its id value

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -42,3 +42,23 @@ router.put('/:id', withAuth, async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+// Delete a post 
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+        if (!postData) {
+            res.status(404).json({ message: 'No post found with this id!' });
+            return;
+        }
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
