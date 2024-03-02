6dc98491715b327ff8a65ff784d7d9dc4f93aa87
akent <alexanderkent099@gmail.com>
2024-03-02 14:55:23 -0500

    Implemented route to handle updating a post by its id value

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -24,3 +24,21 @@ router.post('/', withAuth, async (req, res) => {
         res.status(400).json(err);
     }
 });
+
+// Update a post by its id value 
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+        const postUpdate = await Post.update(req.body, {
+            where: {
+                id: req.params.id,
+            },
+        });
+        if (!postUpdate) {
+            res.status(404).json({ message: 'No post found with this id!' });
+            return;
+        }
+        res.status(200).json(postUpdate);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
