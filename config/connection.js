3e51eb444db2db3a411d8f296cdcf59cc326ab8d
akent <alexanderkent099@gmail.com>
2024-03-05 16:26:27 -0500

    Adding config to allow JAWSDB_URL for deployment to Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-// const env = process.env.NODE_ENV || 'development';
-const config = require('./config.js');
+let sequelize;
 
-const sequelize = new Sequelize(config.database, config.username, config.password, {
-    host: config.host,
-    dialect: config.dialect,
-});
+if (process.env.JAWSDB_URL) {
+    // If running on Heroku with JawsDB MySQL, use the JawsDB URL
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    // Fallback to separate environment variables for local development
+    const config = require('./config.js');
+    sequelize = new Sequelize(config.database, config.username, config.password, {
+        host: config.host,
+        dialect: config.dialect,
+    });
+}
 
 module.exports = sequelize;
