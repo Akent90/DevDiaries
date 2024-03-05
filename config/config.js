2c3db7e5957dadb1d45eddc333366f21263b33bf
akent <alexanderkent099@gmail.com>
2024-03-05 15:20:58 -0500

    Removing developemnt, test, and production environments from connection

--- config/config.js
+++ config/config.js
@@ -1,17 +1,26 @@
 require('dotenv').config();
 
 module.exports = {
-    development: {
-        database: process.env.DB_NAME,
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        host: process.env.DB_HOST,
-        dialect: 'mysql',
-    },
-    test: {
-        // Test environemnt config
-    },
-    production: {
-        use_env_variable: 'DATABASE_URL',
-    }
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    dialect: 'mysql',
 };
+
+// require('dotenv').config();
+
+// module.exports = {
+//     development: {
+//         database: process.env.DB_NAME,
+//         username: process.env.DB_USER,
+//         password: process.env.DB_PASSWORD,
+//         host: process.env.DB_HOST,
+//         dialect: 'mysql',
+//     },
+//     test: {
+//         // Test environment config
+//     },
+//     production: {
+//         use_env_variable: 'DATABASE_URL',
+//     }
+// };
--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,12 @@
 const Sequelize = require('sequelize');
-require('dotenv').config(); // Ensure the environment variables are loaded
+require('dotenv').config();
 
-const env = process.env.NODE_ENV || 'development';
-const config = require('./config.js')[env];
+// const env = process.env.NODE_ENV || 'development';
+const config = require('./config.js');
 
-let sequelize;
-
-if (config.use_env_variable) {
-    sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-    sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
+    dialect: config.dialect,
+});
 
 module.exports = sequelize;
